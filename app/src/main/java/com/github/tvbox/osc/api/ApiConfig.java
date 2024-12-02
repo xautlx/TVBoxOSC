34b34827e11de1df764e5ac4e0c23131cbd7ee22
LiXia <xautlx@hotmail.com>
2024-12-02 09:01:23 +0800

    预置播放源

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -129,33 +129,43 @@ public class ApiConfig {
         return "".getBytes();
     }
 
+    private String initApiUrl(LoadConfigCallback callback, Activity activity) {
+        String appid = "12345678900";
+        try {
+            Context context = activity.getApplicationContext();
+            String idFilePath = context.getFilesDir() + "/" + "tv.id";
+            File idFile = new File(idFilePath);
+            if (idFile.exists()) {
+                appid = FileUtils.readFromFile(context, idFilePath);
+            } else {
+                Date date = new Date();
+                SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+                Random random = new Random();
+                int randomNumber = random.nextInt(900) + 100;
+                appid = format.format(date) + randomNumber;
+                FileUtils.writeToFile(context, idFilePath, appid);
+            }
+        } catch (Exception e) {
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            StringBuilder stackTraceStr = new StringBuilder();
+            for (StackTraceElement element : stackTrace) {
+                stackTraceStr.append(element.toString()).append("\n");
+            }
+            String detailedText = stackTraceStr.toString();
+            callback.error(detailedText);
+        }
+        return "http://hub.entdiy.xyz/tv/" + appid + "/api.json";
+    }
+
+    public static void main(String[] args) {
+        new ApiConfig().initApiUrl(null, null);
+    }
+
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
         // Embedded Source : Update in Strings.xml if required
         String _apiUrl = Hawk.get(HawkConfig.API_URL, HomeActivity.getRes().getString(R.string.app_source));
         if (_apiUrl.isEmpty()) {
-            String appid = "12345678900";
-            try {
-                Context context = activity.getApplicationContext();
-                String idFilePath = context.getFilesDir() + "/" + "tv.id";
-                callback.error(idFilePath);
-                File idFile = new File(idFilePath);
-                if (idFile.exists()) {
-                    appid = FileUtils.readFromFile(context, idFilePath);
-                    callback.error("readFromFile" + appid);
-                } else {
-                    Date date = new Date();
-                    SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-                    Random random = new Random();
-                    int randomNumber = random.nextInt(900) + 100;
-                    appid = format.format(date) + randomNumber;
-                    callback.error("writeToFile" + appid);
-                    FileUtils.writeToFile(context, idFilePath, appid);
-                }
-            } catch (Exception e) {
-                callback.error(e.fillInStackTrace().getMessage());
-            }
-            _apiUrl = "http://hub.entdiy.xyz/tv/" + appid + "/api.json";
-            callback.error(_apiUrl);
+            _apiUrl = initApiUrl(callback, activity);
         }
         final String apiUrl = _apiUrl;
         File cache = new File(App.getInstance().getFilesDir().getAbsolutePath() + "/" + MD5.encode(apiUrl));
